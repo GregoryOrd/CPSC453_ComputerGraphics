5b56852ce34f368f59a71d0622d62ab44b48d40f
gregoryord <g.ord@hotmail.ca>
2021-11-25 21:17:41 -0700

    Added animation status text to top left of the screen

--- Assmt4/453-skeleton-A4/453-skeleton-A4/453-skeleton/main.cpp
+++ Assmt4/453-skeleton-A4/453-skeleton-A4/453-skeleton/main.cpp
@@ -17,6 +17,10 @@
 #include "Window.h"
 #include "Camera.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 #include "glm/glm.hpp"
 #include <glm/gtx/transform.hpp >
 #include "glm/gtc/type_ptr.hpp"
@@ -421,6 +425,46 @@ int main() {
 		}
 
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
+
+		// Starting the new ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		// Putting the text-containing window in the top-left of the screen.
+		ImGui::SetNextWindowPos(ImVec2(5, 5));
+
+		// Setting flags
+		ImGuiWindowFlags textWindowFlags =
+			ImGuiWindowFlags_NoMove |				// text "window" should not move
+			ImGuiWindowFlags_NoResize |				// should not resize
+			ImGuiWindowFlags_NoCollapse |			// should not collapse
+			ImGuiWindowFlags_NoSavedSettings |		// don't want saved settings mucking things up
+			ImGuiWindowFlags_AlwaysAutoResize |		// window should auto-resize to fit the text
+			ImGuiWindowFlags_NoBackground |			// window should be transparent; only the text should be visible
+			ImGuiWindowFlags_NoDecoration |			// no decoration; only the text should be visible
+			ImGuiWindowFlags_NoTitleBar;			// no title; only the text should be visible
+
+		// Begin a new window with these flags. (bool *)0 is the "default" value for its argument.
+		ImGui::Begin("scoreText", (bool*)0, textWindowFlags);
+
+		// Scale up text a little, and set its value
+		ImGui::SetWindowFontScale(2.5f);
+
+		if (animating)
+		{
+			ImGui::Text("Animation Status: Running");
+		}
+		else
+		{
+			ImGui::Text("Animation Status: Paused");
+		}
+
+		// End the window
+		ImGui::End();
+
+		ImGui::Render();	// Render the ImGui window
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData()); // Some middleware thing
+
 
 		window.swapBuffers();
 	}
